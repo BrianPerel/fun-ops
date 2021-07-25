5980ca2ce1ad9f66d1a7934a3136739e1e9b8da6
Brian <brianperel@comcast.net>
2021-07-25 12:50:55 -0400

    Additional program enhancements

--- fun-ops/src/guess/the/number/GuessingGame.java
+++ fun-ops/src/guess/the/number/GuessingGame.java
@@ -1,4 +1,4 @@
-package guess.remainder;
+package guess.the.number;
 
 import java.awt.Color;
 import java.awt.EventQueue;
--- fun-ops/src/wiggle/mouse/MouseMove.java
+++ fun-ops/src/wiggle/mouse/MouseMove.java
@@ -8,10 +8,15 @@ import java.awt.event.ActionListener;
 import java.util.Random;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
- * Mouse move program. Moves the mouse to random coordinate points on the screen every 4 seconds until STOP button is pressed.
+ * Mouse move program. Moves the mouse to random coordinate points on the screen
+ * every 4 seconds until STOP button is pressed. Useful for when you want to be
+ * away from your computer however appear to be available
+ * 
  * @author Brian Perel
  *
  */
@@ -20,10 +25,12 @@ public class MouseMove implements ActionListener {
 	private JFrame frame;
 	JButton btnStart;
 	JButton btnStop;
-	static boolean performAction;
-	private static final long FOUR_SECONDS = 4000;
+	static boolean performAction = false;
 	private static final int SIX_HUNDRED = 600;
 	static Random random = new Random();
+	static int timeToWait;
+	final JComboBox<String> comboBox;
+	String[] choices = { "1/2 minute", "1 minute", "3 minutes", "5 minutes", "10 minutes" };
 
 	/**
 	 * Launch the application.
@@ -61,18 +68,29 @@ public class MouseMove implements ActionListener {
 	 */
 	private MouseMove() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 176, 148);
+		frame.setBounds(100, 100, 430, 182);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		btnStart = new JButton("START");
-		btnStart.setBounds(45, 43, 91, 23);
+		btnStart.setBounds(88, 82, 91, 23);
 		frame.getContentPane().add(btnStart);
 		btnStart.addActionListener(this);
 
 		btnStop = new JButton("STOP");
-		btnStop.setBounds(177, 43, 91, 23);
+		btnStop.setBounds(220, 82, 91, 23);
 		frame.getContentPane().add(btnStop);
+
+		JLabel lblNewLabel = new JLabel("Time to wait before mouse move (in minutes)");
+		lblNewLabel.setBounds(23, 23, 264, 23);
+		frame.getContentPane().add(lblNewLabel);
+
+		// JComboBox comboBox = new JComboBox(); // use this when switching to
+		// WindowBuilder editor's design tab since "new JComboBox<>() is not valid
+		// choice for component creation"
+		comboBox = new JComboBox<>(choices);
+		comboBox.setBounds(297, 23, 83, 22);
+		frame.getContentPane().add(comboBox);
 		btnStop.addActionListener(this);
 	}
 
@@ -80,6 +98,29 @@ public class MouseMove implements ActionListener {
 	public void actionPerformed(ActionEvent ae) {
 		if (ae.getSource() == btnStart) {
 			performAction = true;
+
+			switch ((String) comboBox.getSelectedItem()) {
+			case "1/2 minute":
+				timeToWait = 30000;
+				break;
+
+			case "1 minute":
+				timeToWait = 60000;
+				break;
+
+			case "3 minutes":
+				timeToWait = 180000;
+				break;
+
+			case "5 minutes":
+				timeToWait = 300000;
+				break;
+
+			case "10 minutes":
+				timeToWait = 600000;
+				break;
+			}
+
 		} else if (ae.getSource() == btnStop) {
 			performAction = false;
 		}
@@ -89,8 +130,8 @@ public class MouseMove implements ActionListener {
 		Robot rb = new Robot();
 
 		while (performAction) {
+			Thread.sleep(timeToWait);
 			rb.mouseMove(random.nextInt(SIX_HUNDRED), random.nextInt(SIX_HUNDRED));
-			Thread.sleep(FOUR_SECONDS);
 		}
 	}
 }
